15fefb95c1acefd502fc87f5f4c194bae77e79d7
Alexandre HERGAUX <alexandre.hergaux@malakoffhumanis.com>
2025-08-04 23:35:51 +0200

    fix bug checkbox again

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ function afficherSelection() {
     const checkboxes = document.querySelectorAll('input[name="activity"]:checked');
     const resultat = document.getElementById('resultat');
     
+    if (date.length === 0) {
+            resultat.textContent = "Aucune date sélectionnée.";
+            return;
+        }
+
     if (checkboxes.length === 0) {
         resultat.textContent = "Aucune activité sélectionnée.";
         return;
     }
 
-    const selections = Array.from(checkboxes).map(cb => cb.value);
-    resultat.textContent = date + ', je veux bien faire :' + selections.join(", ");
+    const selections1 = Array.from(date).map(cb => cb.value);
+
+    const selections2 = Array.from(checkboxes).map(cb => cb.value);
+    resultat.textContent = selections1.join(", ") + ', je veux bien faire : ' + selections2.join(", ");
 }
